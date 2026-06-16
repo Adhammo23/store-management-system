e012eb9b4384dc8ea9c46a51d67008b9fe25dde3
Adhammo23 <Adhammo23@users.noreply.github.com>
2026-06-16 14:07:05 +0300

    feat: update application.properties and modify SecurityConfig for Swagger UI access

--- src/main/java/com/adham/store_management_system/security/SecurityConfig.java
+++ src/main/java/com/adham/store_management_system/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(("/api/auth/**")).permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
